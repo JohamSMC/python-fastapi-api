ddfe07064780e9ca21110e650758a8149bbdff45
JohamSMC <joham.smc@gmail.com>
2024-09-01 08:09:38 -0500

    [Feat] test main base path

--- app/main.py
+++ app/main.py
@@ -1,22 +1,26 @@
 import time
-from typing import Union
 from datetime import datetime
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
+    return "Hello World JohamSMC"
+
+@app.get("/time")
+def get_time():
     return f"Hello World {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 
+
 @app.get("/sleep/{time_sleep}")
-def endpoint_sleep(time_sleep: int):
+def get_endpoint_sleep(time_sleep: int):
     start_time = datetime.now()
     print(f"start time: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
     time.sleep(time_sleep)
     end_time = datetime.now()
     print(f"end time  : {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
-    time_difference = end_time-start_time
-    print(f"Duracion endPoint : {time_difference.total_seconds()}")
-    return f"Duracion endPoint : {time_difference.total_seconds()}"
+    time_difference = end_time - start_time
+    print(f"Duración endPoint : {time_difference.total_seconds()}")
+    return f"Duración endPoint : {time_difference.total_seconds()}"
